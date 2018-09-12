bb40a7befa0efcc0e914133b0af6266a42426629
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-09-12 16:00:21 -0700

    refine

--- R/weight_by_empirical_counts.R
+++ R/weight_by_empirical_counts.R
@@ -4,12 +4,12 @@
 
 #' @title Weight by empirical counts
 #'
-#' @param total Vector containing coverage per variant
+#' @param total integer vector containing coverage per variant
 #' @return A one-column matrix of weights for each coverage level
 #' @export
 weight_by_empirical_counts <- function(total) {
   t <- as.data.frame(table(total), stringsAsFactors = FALSE)
-  w <- matrix(0, max(total), 1)
+  w <- matrix(0, max(total, na.rm = TRUE), 1)
   for (j in 1:nrow(t)) {
     w[as.integer(t[j, 1]), 1] <- t[j, 2]
   }
